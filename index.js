a5ce0816fe0d95ecf3d9320ee86c8424c797cedd
Anni Kynsilehto <anni.kynsilehto@gmail.com>
2020-02-05 20:15:01 +0200

    Tehtävät 3.3-3.6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+app.use(express.json()) 
 
 let persons = [
   {
@@ -24,15 +25,65 @@ let persons = [
   },
 ]
 
-let length = persons.length
-
 app.get("/info", (req, res) => {
-  const information = `<p>Phonebook has ${length} numbers.</p><p>${new Date().toString()}</p>`
+  const information = `<p>Phonebook has ${
+    persons.length
+  } numbers.</p><p>${new Date().toString()}</p>`
   res.send(information)
 })
 
 app.get("/api/persons", (req, res) => {
   res.json(persons)
+})
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: "Name missing",
+    })
+  }
+
+  if (!body.number) {
+    return res.status(400).json({
+      error: "Number missing",
+    })
+  }
+
+  const previousEntry = persons.find(person => person.name === body.name)
+  if (previousEntry) {
+    return res.status(400).json({
+      error: "A person with that name has already been added",
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * Math.floor(1000)),
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
 })
 
 const PORT = 3001
