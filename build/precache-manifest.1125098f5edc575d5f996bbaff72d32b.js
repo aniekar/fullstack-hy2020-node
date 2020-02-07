c53e768891a3b2147bce60122dbac25cf4920144
Anni Kynsilehto <anni.kynsilehto@gmail.com>
2020-02-07 07:54:53 +0200

    Frontin build-kansio lisätty ja muokattu skriptejä

---
+++ build/precache-manifest.1125098f5edc575d5f996bbaff72d32b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75393660bf9ee883f697008e1d85d4e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4dcf438a57b08fe6d44",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8514db5cee128984cd03",
+    "url": "/static/js/2.16d1eaee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.16d1eaee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4dcf438a57b08fe6d44",
+    "url": "/static/js/main.bea3686d.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
---
+++ build/precache-manifest.a6136244a316297c1d4752ffe2be38ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae654def2b6ffc614303ef5894dbed6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa44a5ae104bfe44cee",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8514db5cee128984cd03",
+    "url": "/static/js/2.16d1eaee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.16d1eaee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa44a5ae104bfe44cee",
+    "url": "/static/js/main.1a6ea6fc.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
