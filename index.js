6aaac79be761a7b80a2931a96c98b60ea055075e
Anni Kynsilehto <anni.kynsilehto@gmail.com>
2020-02-07 18:56:16 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express")
-const app = express()
 const cors = require("cors")
 const morgan = require("morgan")
+require("dotenv").config()
+const Person = require("./models/person")
 
+const app = express()
 app.use(express.static("build"))
 app.use(cors())
 app.use(express.json())
@@ -16,7 +18,7 @@ morgan.token("data", req => {
 })
 
 //app.use(morgan("tiny"))
-app.use(morgan(':method :url :status :response-time ms :data'))
+app.use(morgan(":method :url :status :response-time ms :data"))
 
 let persons = [
   {
@@ -49,17 +51,15 @@ app.get("/info", (req, res) => {
 })
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons)
+  Person.find({}).then(people => {
+    res.json(people.map(person => person.toJSON()))
+  })
 })
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  Person.findById(req.params.id).then(person => {
+    res.json(person.toJSON)
+  })
 })
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -67,6 +67,24 @@ app.delete("/api/persons/:id", (req, res) => {
   persons = persons.filter(person => person.id !== id)
 
   res.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON())
+  })
 })
 
 app.post("/api/persons", (req, res) => {
@@ -84,22 +102,21 @@ app.post("/api/persons", (req, res) => {
     })
   }
 
-  const previousEntry = persons.find(person => person.name === body.name)
-  if (previousEntry) {
-    return res.status(400).json({
-      error: "A person with that name has already been added",
-    })
-  }
+  // const previousEntry = persons.find(person => person.name === body.name)
+  // if (previousEntry) {
+  //   return res.status(400).json({
+  //     error: "A person with that name has already been added",
+  //   })
+  // }
 
-  const person = {
+  const person = new Person ({
     name: body.name,
-    number: body.number,
-    id: Math.floor(Math.random() * Math.floor(1000)),
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person)
-
-  res.json(person)
+  person.save().then(savedPerson => {
+    res.json(savedPerson.toJSON())
+  })
 })
 
 const PORT = process.env.PORT || 3001
